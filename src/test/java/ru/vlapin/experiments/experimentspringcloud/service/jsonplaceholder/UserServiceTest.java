3c01663d8636cca8f90bb4b3925b94c04db1683d
VLapin <Vyacheslav.Lapin@gmail.com>
2022-06-01 15:22:27 +0300

    OpenFeign test

--- src/test/java/ru/vlapin/experiments/experimentspringcloud/service/jsonplaceholder/UserServiceTest.java
+++ src/test/java/ru/vlapin/experiments/experimentspringcloud/service/jsonplaceholder/UserServiceTest.java
@@ -1,4 +1,37 @@
+package ru.vlapin.experiments.experimentspringcloud.service.jsonplaceholder;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import ru.vlapin.experiments.experimentspringcloud.model.jsonplaceholder.Address;
+import ru.vlapin.experiments.experimentspringcloud.model.jsonplaceholder.Geo;
+import ru.vlapin.experiments.experimentspringcloud.model.jsonplaceholder.User;
+
+@SpringBootTest
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 class UserServiceTest {
 
+  UserService userService;
+
+  @Test
+  @SneakyThrows
+  @DisplayName("User extracts correctly")
+  void userExtractsCorrectlyTest() {
+    assertThat(userService.all()).isNotNull()
+        .hasSize(10);
+
+    assertThat(userService.findById(1L)).isNotNull()
+        .extracting(ResponseEntity::getBody)
+        .extracting(User::getAddress)
+        .extracting(Address::getGeo)
+        .extracting(Geo::getLatitude, Geo::getLongitude)
+        .contains(-37.3159, 81.1496);
+  }
 }
